663585375fa4916d74c33876e80fa07290000322
tnxnzk123 <tnxnzk123@gmail.com>
2026-07-07 16:07:51 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   // ✅ Google Apps Script 웹앱 URL (배포 완료)
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx3sT4RsxvXdpEoPkKfrudXVXsEQ8KQ1h22C8fvCt4GkU3dPe2JYYt-BGBZmfyODILxeg/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyjHgc9etBxEtoBIVu2yESoUFhAOsOOvKo5seuRjuBs7jwQ3Tk--AxUd29u5g16RdaGiA/exec",
 
   // ✅ GitHub Pages 웹앱 주소 (배포 완료)
   APP_BASE_URL: "https://tnxnzk123.github.io/instructor-schedule",
